a9846502820a7e35775c1707f060af3a6bf8f3ba
mtzcpd1724 <mohsanabbas@cvccorp.com.br>
2020-03-03 14:34:53 -0300

    collection route is done

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  selector: "app-collections-home",
+  templateUrl: "./collections-home.component.html",
+  styleUrls: ["./collections-home.component.css"]
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: "Mosh", age: 23, job: "designer", employed: true },
+    { name: "Elyse", age: 33, job: "Developer", employed: true },
+    { name: "Jill", age: 31, job: "Developer", employed: false }
+  ];
+  headers = [
+    { key: "employed", label: "has a job" },
+    {
+      key: "name",
+      label: "name"
+    },
+    {
+      key: "age",
+      label: "age"
+    },
+    {
+      key: "job",
+      label: "job"
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
